51799b5213fb505ddc2b9e3257e162e0aad12460
Samuel Ajayi <sirmisell@gmail.com>
2026-05-04 22:31:11 +0100

    fix: remove incompatible planetscale adapter and fix db pool error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
-import mysql from 'mysql2/promise'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const connectionString = process.env.DATABASE_URL
-
-// Create a connection pool
-const pool = mysql.createPool({
-  uri: connectionString,
-})
-
-// Create the adapter
-const adapter = new PrismaPlanetScale(pool)
-
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
     log: ['query'],
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
